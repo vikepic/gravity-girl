4247a431eb99901acf3e2a58519f803cb49a0288
wextia <valcazar96@gmail.com>
2017-04-22 12:21:57 +0200

    WIP

--- Assets/scripts/FlyingController.cs
+++ Assets/scripts/FlyingController.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class FlyingController : StateController {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    GameObject gravityObjectPrefab;
+
+    GameObject playerGravityObject;
+
+    private void OnEnable()
+    {
+        // We build the playerGravityObject
+        if (playerGravityObject == null)
+        {
+            playerGravityObject =
+                Instantiate(gravityObjectPrefab,
+                transform.position,
+                transform.rotation,
+                gameObject.transform);
+
+            playerGravityObject.AddComponent<
+                PlayerGravityObjectController>();
+        }
+
+        playerGravityObject.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        if (playerGravityObject != null)
+        {
+            playerGravityObject.SetActive(false);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
---
+++ Assets/scripts/PlayerGravityObjectController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGravityObjectController : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        SyncPlayer();
+    }
+
+    void SyncPlayer()
+    {
+        // Very VERY ugly stuff where i sync the parent's position
+        // with mine in order to create a gravitational pull effect
+        // on the player
+        transform.parent.position = transform.position;
+        transform.localPosition = Vector3.zero;
+        transform.parent.rotation = transform.rotation;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Uncomment to get some useful debugging info
+        //Debug.Log("I collided with " + collision.gameObject.name +
+        //    "!");
+        // TODO
+        transform.parent.SendMessage("OnPlanetCollision");
+    }
+}
